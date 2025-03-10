f9433349e387c26338d357d9fb020acb0b5cbbf8
Karlos H. Braga <karlos.hbraga@gmail.com>
2025-03-10 00:50:50 +0000

    Include library finished

---
+++ development/src/LibraryCSharpColorTerminal/ConsoleCSharpColorTerminal/Program.cs
@@ -0,0 +1,16 @@
+﻿using Konkah.LibraryCSharpColorTerminal;
+
+namespace ConsoleCSharpColorTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String exemplo = "exemplo vermelho";
+            String exemplo1 = "exemplo azul";
+
+            ConsoleColored.WriteLine(exemplo, ConsoleColor.Red);
+            ConsoleColored.WriteLine(exemplo1, ConsoleColor.Blue);
+        }
+    }
+}
---
+++ development/src/LibraryCSharpColorTerminal/LibraryCSharpColorTerminal/ConsoleColored.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konkah.LibraryCSharpColorTerminal
+{
+    public class ConsoleColored
+    {
+        public static void WriteLine(string exemplo, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(exemplo);
+            Console.ResetColor();
+        }
+    }
+}
